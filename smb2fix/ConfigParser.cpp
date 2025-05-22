6035f93a74de13c21e89df9e641764e9b91f3b16
Producks <davetrap.demers@gmail.com>
2025-05-21 23:28:56 -0400

    config parser done

--- smb2fix/ConfigParser.cpp
+++ smb2fix/ConfigParser.cpp
@@ -8,7 +8,27 @@
 #include <map>
 #include <functional>
 
+
 static void set_bool_config(bool &config, const std::string &value) { config = value == "1"; }
+
+static void set_string_config(std::string &result, bool &set, const std::string &value) {
+  if (set) {
+    std::size_t first_quote = value.find('\"');
+    std::size_t second_quote = value.rfind('\"');
+    if (first_quote == std::string::npos || second_quote == std::string::npos || first_quote == second_quote) {
+      Io::print_error_message("When reading the file output name");
+      set = false;
+    }
+    else {
+      result = value.substr(first_quote + 1, second_quote - (first_quote + 1));
+      if (result.empty()) {
+        Io::print_error_message("The file output name can't be empty");
+        set = false;
+      }
+      std::cout << "result:" << result << std::endl;
+    }
+  }
+}
 
 static std::map<const std::string, std::function<void(Config&, const std::string&)>> func_map { 
   {"Colorfix", [](Config &config, const std::string &value) {set_bool_config(config.color_fix, value);}},
@@ -17,7 +37,8 @@ static std::map<const std::string, std::function<void(Config&, const std::string
   {"Codeinjection", [](Config &config, const std::string &value) {set_bool_config(config.code_injection, value);}},
   {"Alwaysoverwrite", [](Config &config, const std::string &value) {set_bool_config(config.overwrite_file, value);}},
   {"Samefileoutput", [](Config &config, const std::string &value) {set_bool_config(config.same_file_out, value);}},
-  {"Skipenteronsuccess", [](Config &config, const std::string &value) {set_bool_config(config.skip_enter_on_success, value);}}
+  {"Skipenteronsuccess", [](Config &config, const std::string &value) {set_bool_config(config.skip_enter_on_success, value);}},
+  {"Savefilename", [](Config &config, const std::string &value) {set_string_config(config.output_name, config.same_file_out, value);}}
 };
 
 static void sanitize_line(std::string &line) {
@@ -27,8 +48,8 @@ static void sanitize_line(std::string &line) {
   line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
   if (line.empty())
     return;
-  std::size_t seperator = line.find('=');
-  if (seperator == std::string::npos || line.begin() + seperator + 1 == line.end()) {
+  std::size_t separator = line.find('=');
+  if (separator == std::string::npos || line.begin() + separator + 1 == line.end()) {
     Io::print_error_message("With config line \"" + line + "\" it will be ignored");
     line.clear();
   }
@@ -39,12 +60,12 @@ void ConfigParser::ParseConfigFile(Config &config) {
   std::string line;
 
   if (!std::filesystem::exists("config.txt")) {
-    std::cerr << "Couldn't locate config file" << std::endl;
+    Io::print_error_message("Couldn't locate config file");
     return;
   }
   config_file.open("config.txt");
   if (config_file.fail()) {
-    std::cerr << "Error when opening the file" << std::endl;
+    Io::print_error_message("When opening the config file");
     return;
   }
   while (std::getline(config_file, line)) {
@@ -55,5 +76,4 @@ void ConfigParser::ParseConfigFile(Config &config) {
     if (func_map.find(splice.first) != func_map.end())
       func_map[splice.first](config, splice.second);
   }
-  std::cout << config.overwrite_file << std::endl;
 }
